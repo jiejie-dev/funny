9197a04dc88530743176d7196378868bd391f7df
jiejie-dev <1006397539@qq.com>
2026-07-01 05:16:48 +0800

    v2: AST statement nodes

--- v2/internal/ast/ast.go
+++ v2/internal/ast/ast.go
@@ -177,3 +177,161 @@ func joinComma(parts []string) string {
 	}
 	return out
 }
+
+// ----- Statements -----
+
+type Block struct {
+	NodePos    Pos
+	Statements []Statement
+}
+
+func (s *Block) Pos() Pos    { return s.NodePos }
+func (s *Block) stmtMarker() {}
+func (s *Block) nodeMarker() {}
+func (s *Block) String() string {
+	out := ""
+	for _, s := range s.Statements {
+		out += s.String() + "\n"
+	}
+	return out
+}
+
+type LetStmt struct {
+	NodePos Pos
+	Name    string
+	TypeAnn string
+	Value   Expression
+}
+
+func (s *LetStmt) Pos() Pos    { return s.NodePos }
+func (s *LetStmt) stmtMarker() {}
+func (s *LetStmt) nodeMarker() {}
+func (s *LetStmt) String() string {
+	if s.TypeAnn != "" {
+		return fmt.Sprintf("let %s: %s = %s", s.Name, s.TypeAnn, s.Value.String())
+	}
+	return fmt.Sprintf("let %s = %s", s.Name, s.Value.String())
+}
+
+type AssignStmt struct {
+	NodePos Pos
+	Target  Expression
+	Value   Expression
+}
+
+func (s *AssignStmt) Pos() Pos    { return s.NodePos }
+func (s *AssignStmt) stmtMarker() {}
+func (s *AssignStmt) nodeMarker() {}
+func (s *AssignStmt) String() string {
+	return fmt.Sprintf("%s = %s", s.Target.String(), s.Value.String())
+}
+
+type IfStmt struct {
+	NodePos   Pos
+	Cond      Expression
+	Then      *Block
+	ElseIf    *IfStmt
+	ElseBlock *Block
+}
+
+func (s *IfStmt) Pos() Pos    { return s.NodePos }
+func (s *IfStmt) stmtMarker() {}
+func (s *IfStmt) nodeMarker() {}
+func (s *IfStmt) String() string {
+	out := fmt.Sprintf("if %s:\n%s", s.Cond.String(), s.Then.String())
+	if s.ElseIf != nil {
+		out += "elif " + s.ElseIf.String()
+	}
+	if s.ElseBlock != nil {
+		out += "else:\n" + s.ElseBlock.String()
+	}
+	return out
+}
+
+type ForStmt struct {
+	NodePos  Pos
+	Name     string
+	Iterable Expression
+	Body     *Block
+}
+
+func (s *ForStmt) Pos() Pos    { return s.NodePos }
+func (s *ForStmt) stmtMarker() {}
+func (s *ForStmt) nodeMarker() {}
+func (s *ForStmt) String() string {
+	return fmt.Sprintf("for %s in %s:\n%s", s.Name, s.Iterable.String(), s.Body.String())
+}
+
+type WhileStmt struct {
+	NodePos Pos
+	Cond    Expression
+	Body    *Block
+}
+
+func (s *WhileStmt) Pos() Pos    { return s.NodePos }
+func (s *WhileStmt) stmtMarker() {}
+func (s *WhileStmt) nodeMarker() {}
+func (s *WhileStmt) String() string {
+	return fmt.Sprintf("while %s:\n%s", s.Cond.String(), s.Body.String())
+}
+
+type MatchArm struct {
+	Pattern Expression
+	Body    *Block
+}
+
+type MatchStmt struct {
+	NodePos Pos
+	Expr    Expression
+	Arms    []MatchArm
+}
+
+func (s *MatchStmt) Pos() Pos    { return s.NodePos }
+func (s *MatchStmt) stmtMarker() {}
+func (s *MatchStmt) nodeMarker() {}
+func (s *MatchStmt) String() string {
+	out := fmt.Sprintf("match %s:\n", s.Expr.String())
+	for _, a := range s.Arms {
+		out += fmt.Sprintf("    %s =>\n%s", a.Pattern.String(), a.Body.String())
+	}
+	return out
+}
+
+type ReturnStmt struct {
+	NodePos Pos
+	Value   Expression
+}
+
+func (s *ReturnStmt) Pos() Pos    { return s.NodePos }
+func (s *ReturnStmt) stmtMarker() {}
+func (s *ReturnStmt) nodeMarker() {}
+func (s *ReturnStmt) String() string {
+	if s.Value == nil {
+		return "return"
+	}
+	return fmt.Sprintf("return %s", s.Value.String())
+}
+
+type BreakStmt struct{ NodePos Pos }
+
+func (s *BreakStmt) Pos() Pos       { return s.NodePos }
+func (s *BreakStmt) stmtMarker()    {}
+func (s *BreakStmt) nodeMarker()    {}
+func (s *BreakStmt) String() string { return "break" }
+
+type ContinueStmt struct{ NodePos Pos }
+
+func (s *ContinueStmt) Pos() Pos       { return s.NodePos }
+func (s *ContinueStmt) stmtMarker()    {}
+func (s *ContinueStmt) nodeMarker()    {}
+func (s *ContinueStmt) String() string { return "continue" }
+
+type ExprStmt struct {
+	NodePos Pos
+	X       Expression
+}
+
+func (s *ExprStmt) Pos() Pos       { return s.NodePos }
+func (s *ExprStmt) stmtMarker()    {}
+func (s *ExprStmt) nodeMarker()    {}
+func (s *ExprStmt) String() string { return s.X.String() }
---
+++ v2/internal/ast/statement_test.go
@@ -0,0 +1,101 @@
+package ast
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBlock_String(t *testing.T) {
+	b := &Block{Statements: []Statement{
+		&LetStmt{Name: "x", Value: &LiteralExpr{Value: 1}},
+		&ExprStmt{X: &VariableExpr{Name: "x"}},
+	}}
+	s := b.String()
+	assert.Contains(t, s, "let x = 1")
+	assert.Contains(t, s, "x")
+}
+
+func TestLetStmt_String(t *testing.T) {
+	s := &LetStmt{Name: "x", Value: &LiteralExpr{Value: 1}}
+	assert.Equal(t, "let x = 1", s.String())
+}
+
+func TestLetStmt_StringWithType(t *testing.T) {
+	s := &LetStmt{Name: "x", TypeAnn: "int", Value: &LiteralExpr{Value: 1}}
+	assert.Equal(t, "let x: int = 1", s.String())
+}
+
+func TestAssignStmt_String(t *testing.T) {
+	s := &AssignStmt{Target: &VariableExpr{Name: "x"}, Value: &LiteralExpr{Value: 2}}
+	assert.Equal(t, "x = 2", s.String())
+}
+
+func TestIfStmt_String(t *testing.T) {
+	s := &IfStmt{
+		Cond: &VariableExpr{Name: "x"},
+		Then: &Block{Statements: []Statement{
+			&LetStmt{Name: "y", Value: &LiteralExpr{Value: 1}},
+		}},
+	}
+	out := s.String()
+	assert.Contains(t, out, "if x:")
+	assert.Contains(t, out, "let y = 1")
+}
+
+func TestIfStmt_StringWithElse(t *testing.T) {
+	s := &IfStmt{
+		Cond:      &VariableExpr{Name: "x"},
+		Then:      &Block{Statements: []Statement{&LetStmt{Name: "a", Value: &LiteralExpr{Value: 1}}}},
+		ElseBlock: &Block{Statements: []Statement{&LetStmt{Name: "b", Value: &LiteralExpr{Value: 2}}}},
+	}
+	out := s.String()
+	assert.Contains(t, out, "if x:")
+	assert.Contains(t, out, "else:")
+	assert.Contains(t, out, "let a = 1")
+	assert.Contains(t, out, "let b = 2")
+}
+
+func TestForStmt_String(t *testing.T) {
+	s := &ForStmt{
+		Name:     "i",
+		Iterable: &VariableExpr{Name: "items"},
+		Body:     &Block{Statements: []Statement{&ExprStmt{X: &VariableExpr{Name: "i"}}}},
+	}
+	out := s.String()
+	assert.Contains(t, out, "for i in items:")
+}
+
+func TestWhileStmt_String(t *testing.T) {
+	s := &WhileStmt{
+		Cond: &VariableExpr{Name: "x"},
+		Body: &Block{Statements: []Statement{&ExprStmt{X: &VariableExpr{Name: "x"}}}},
+	}
+	out := s.String()
+	assert.Contains(t, out, "while x:")
+}
+
+func TestReturnStmt_String(t *testing.T) {
+	s := &ReturnStmt{Value: &LiteralExpr{Value: 1}}
+	assert.Equal(t, "return 1", s.String())
+}
+
+func TestReturnStmt_NoValue(t *testing.T) {
+	s := &ReturnStmt{}
+	assert.Equal(t, "return", s.String())
+}
+
+func TestBreakStmt_String(t *testing.T) {
+	s := &BreakStmt{}
+	assert.Equal(t, "break", s.String())
+}
+
+func TestContinueStmt_String(t *testing.T) {
+	s := &ContinueStmt{}
+	assert.Equal(t, "continue", s.String())
+}
+
+func TestExprStmt_String(t *testing.T) {
+	s := &ExprStmt{X: &VariableExpr{Name: "x"}}
+	assert.Equal(t, "x", s.String())
+}
