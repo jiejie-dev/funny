1591966a003c5139c6c77173f0fae45591e72b74
jiejie-dev <1006397539@qq.com>
2026-07-01 05:50:43 +0800

    v2: CLI with run and ast commands

--- v2/cmd/funny/main.go
+++ v2/cmd/funny/main.go
@@ -1,5 +1,63 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
 
-func main() { fmt.Println("funny v2 (M1)") }
+	"github.com/spf13/cobra"
+
+	"github.com/jerloo/funny/v2/internal/cli"
+)
+
+var version = "0.1.0"
+
+var rootCmd = &cobra.Command{
+	Use:     "funny",
+	Short:   "funny v2 - AI-native scripting language",
+	Version: version,
+}
+
+var runCmd = &cobra.Command{
+	Use:   "run <script>",
+	Short: "Execute a funny script",
+	Args:  cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		data, err := os.ReadFile(args[0])
+		if err != nil {
+			return err
+		}
+		if err := cli.Run(data, args[0]); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		return nil
+	},
+}
+
+var astCmd = &cobra.Command{
+	Use:   "ast <script>",
+	Short: "Print JSON AST",
+	Args:  cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		data, err := os.ReadFile(args[0])
+		if err != nil {
+			return err
+		}
+		out, err := cli.Ast(data, args[0])
+		if err != nil {
+			return err
+		}
+		fmt.Println(string(out))
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(runCmd, astCmd)
+}
+
+func main() {
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
