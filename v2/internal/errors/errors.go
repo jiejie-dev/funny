6b8f36d5d965b40592e18b9f5e75c698c8366704
jiejie-dev <1006397539@qq.com>
2026-07-01 04:21:45 +0800

    v2: add unified error system

---
+++ v2/internal/errors/errors.go
@@ -0,0 +1,33 @@
+package errors
+
+import "fmt"
+
+type Position struct {
+	File string
+	Line int
+	Col  int
+}
+
+type Error struct {
+	Code    string
+	Message string
+	Pos     Position
+	Hint    string
+}
+
+func New(code, message string, pos Position, hint string) *Error {
+	return &Error{Code: code, Message: message, Pos: pos, Hint: hint}
+}
+
+func (e *Error) Error() string {
+	return e.Format()
+}
+
+func (e *Error) Format() string {
+	s := fmt.Sprintf("error[%s]: %s\n --> %s:%d:%d\n",
+		e.Code, e.Message, e.Pos.File, e.Pos.Line, e.Pos.Col)
+	if e.Hint != "" {
+		s += fmt.Sprintf("\nhelp: %s", e.Hint)
+	}
+	return s
+}
