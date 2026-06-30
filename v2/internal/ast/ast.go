11cf0f6a48ad3bd17d30249b6d272b7b613307ba
jiejie-dev <1006397539@qq.com>
2026-07-01 05:14:39 +0800

    v2: AST expression nodes

--- v2/internal/ast/ast.go
+++ v2/internal/ast/ast.go
@@ -14,6 +14,7 @@ func (p Pos) String() string {
 
 type Node interface {
 	Pos() Pos
+	String() string
 	nodeMarker()
 }
 
@@ -25,4 +26,154 @@ type Statement interface {
 type Expression interface {
 	Node
 	exprMarker()
+}
+
+// ----- Expressions -----
+
+type LiteralExpr struct {
+	NodePos Pos
+	Value   any
+}
+
+func (e *LiteralExpr) Pos() Pos    { return e.NodePos }
+func (e *LiteralExpr) exprMarker() {}
+func (e *LiteralExpr) nodeMarker() {}
+func (e *LiteralExpr) String() string {
+	if s, ok := e.Value.(string); ok {
+		return fmt.Sprintf("%q", s)
+	}
+	return fmt.Sprintf("%v", e.Value)
+}
+
+type VariableExpr struct {
+	NodePos Pos
+	Name    string
+}
+
+func (e *VariableExpr) Pos() Pos    { return e.NodePos }
+func (e *VariableExpr) exprMarker() {}
+func (e *VariableExpr) nodeMarker() {}
+func (e *VariableExpr) String() string {
+	return e.Name
+}
+
+type BinaryExpr struct {
+	NodePos Pos
+	Left    Expression
+	Op      string
+	Right   Expression
+}
+
+func (e *BinaryExpr) Pos() Pos    { return e.NodePos }
+func (e *BinaryExpr) exprMarker() {}
+func (e *BinaryExpr) nodeMarker() {}
+func (e *BinaryExpr) String() string {
+	return fmt.Sprintf("%s %s %s", e.Left.String(), e.Op, e.Right.String())
+}
+
+type UnaryExpr struct {
+	NodePos Pos
+	Op      string
+	Expr    Expression
+}
+
+func (e *UnaryExpr) Pos() Pos    { return e.NodePos }
+func (e *UnaryExpr) exprMarker() {}
+func (e *UnaryExpr) nodeMarker() {}
+func (e *UnaryExpr) String() string {
+	return fmt.Sprintf("%s %s", e.Op, e.Expr.String())
+}
+
+type SubExpr struct {
+	NodePos Pos
+	Inner   Expression
+}
+
+func (e *SubExpr) Pos() Pos    { return e.NodePos }
+func (e *SubExpr) exprMarker() {}
+func (e *SubExpr) nodeMarker() {}
+func (e *SubExpr) String() string {
+	return fmt.Sprintf("(%s)", e.Inner.String())
+}
+
+type ListExpr struct {
+	NodePos  Pos
+	Elements []Expression
+}
+
+func (e *ListExpr) Pos() Pos    { return e.NodePos }
+func (e *ListExpr) exprMarker() {}
+func (e *ListExpr) nodeMarker() {}
+func (e *ListExpr) String() string {
+	parts := make([]string, len(e.Elements))
+	for i, el := range e.Elements {
+		parts[i] = el.String()
+	}
+	return "[" + joinComma(parts) + "]"
+}
+
+type IndexExpr struct {
+	NodePos Pos
+	Object  Expression
+	Index   Expression
+}
+
+func (e *IndexExpr) Pos() Pos    { return e.NodePos }
+func (e *IndexExpr) exprMarker() {}
+func (e *IndexExpr) nodeMarker() {}
+func (e *IndexExpr) String() string {
+	return fmt.Sprintf("%s[%s]", e.Object.String(), e.Index.String())
+}
+
+type FieldExpr struct {
+	NodePos Pos
+	Object  Expression
+	Field   string
+}
+
+func (e *FieldExpr) Pos() Pos    { return e.NodePos }
+func (e *FieldExpr) exprMarker() {}
+func (e *FieldExpr) nodeMarker() {}
+func (e *FieldExpr) String() string {
+	return fmt.Sprintf("%s.%s", e.Object.String(), e.Field)
+}
+
+type CallExpr struct {
+	NodePos Pos
+	Func    Expression
+	Args    []Expression
+}
+
+func (e *CallExpr) Pos() Pos    { return e.NodePos }
+func (e *CallExpr) exprMarker() {}
+func (e *CallExpr) nodeMarker() {}
+func (e *CallExpr) String() string {
+	parts := make([]string, len(e.Args))
+	for i, a := range e.Args {
+		parts[i] = a.String()
+	}
+	return fmt.Sprintf("%s(%s)", e.Func.String(), joinComma(parts))
+}
+
+type FStringExpr struct {
+	NodePos Pos
+	Raw     string
+}
+
+func (e *FStringExpr) Pos() Pos    { return e.NodePos }
+func (e *FStringExpr) exprMarker() {}
+func (e *FStringExpr) nodeMarker() {}
+func (e *FStringExpr) String() string {
+	return fmt.Sprintf("f%q", e.Raw)
+}
+
+func joinComma(parts []string) string {
+	out := ""
+	for i, p := range parts {
+		if i > 0 {
+			out += ", "
+		}
+		out += p
+	}
+	return out
 }
---
+++ v2/internal/ast/expression_test.go
@@ -0,0 +1,62 @@
+package ast
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLiteralExpr_String(t *testing.T) {
+	e := &LiteralExpr{Value: 42}
+	assert.Equal(t, "42", e.String())
+}
+
+func TestVariableExpr_String(t *testing.T) {
+	e := &VariableExpr{Name: "foo"}
+	assert.Equal(t, "foo", e.String())
+}
+
+func TestBinaryExpr_String(t *testing.T) {
+	e := &BinaryExpr{Left: &VariableExpr{Name: "a"}, Op: "+", Right: &LiteralExpr{Value: 1}}
+	assert.Equal(t, "a + 1", e.String())
+}
+
+func TestUnaryExpr_String(t *testing.T) {
+	e := &UnaryExpr{Op: "not", Expr: &VariableExpr{Name: "x"}}
+	assert.Equal(t, "not x", e.String())
+}
+
+func TestSubExpr_String(t *testing.T) {
+	e := &SubExpr{Inner: &LiteralExpr{Value: 1}}
+	assert.Equal(t, "(1)", e.String())
+}
+
+func TestListExpr_String(t *testing.T) {
+	e := &ListExpr{Elements: []Expression{&LiteralExpr{Value: 1}, &LiteralExpr{Value: 2}}}
+	assert.Equal(t, "[1, 2]", e.String())
+}
+
+func TestIndexExpr_String(t *testing.T) {
+	e := &IndexExpr{Object: &VariableExpr{Name: "a"}, Index: &LiteralExpr{Value: 0}}
+	assert.Equal(t, "a[0]", e.String())
+}
+
+func TestFieldExpr_String(t *testing.T) {
+	e := &FieldExpr{Object: &VariableExpr{Name: "p"}, Field: "name"}
+	assert.Equal(t, "p.name", e.String())
+}
+
+func TestCallExpr_String(t *testing.T) {
+	e := &CallExpr{Func: &VariableExpr{Name: "f"}, Args: []Expression{&LiteralExpr{Value: 1}}}
+	assert.Equal(t, "f(1)", e.String())
+}
+
+func TestFStringExpr_String(t *testing.T) {
+	e := &FStringExpr{Raw: "hello {name}"}
+	assert.Equal(t, `f"hello {name}"`, e.String())
+}
+
+func TestLiteralExpr_String_StringLiteral(t *testing.T) {
+	e := &LiteralExpr{Value: "hi"}
+	assert.Equal(t, `"hi"`, e.String())
+}
